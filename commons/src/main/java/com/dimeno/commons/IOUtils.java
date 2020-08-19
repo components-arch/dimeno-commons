f9df9640b09ed296b9090e55d45e53cd8d3a8694
wangzhen <lavalike@yeah.net>
2020-08-19 18:00:48 +0800

    添加IOUtils工具类

---
+++ commons/src/main/java/com/dimeno/commons/IOUtils.java
@@ -0,0 +1,31 @@
+package com.dimeno.commons;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * IOUtils
+ * Created by wangzhen on 2020/8/19.
+ */
+public class IOUtils {
+
+    /**
+     * close all streams
+     *
+     * @param closeables closeables
+     */
+    public static void close(Closeable... closeables) {
+        if (closeables != null && closeables.length > 0) {
+            for (Closeable closeable : closeables) {
+                if (closeable != null) {
+                    try {
+                        closeable.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+}
