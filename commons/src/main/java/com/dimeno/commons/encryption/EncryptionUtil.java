bf59144d4fdb2db340adafa1620791961cdb2054
wangzhen <lavalike@yeah.net>
2020-08-19 18:21:47 +0800

    添加数据加密工具类

---
+++ commons/src/main/java/com/dimeno/commons/encryption/EncryptionUtil.java
@@ -0,0 +1,128 @@
+package com.dimeno.commons.encryption;
+
+import android.util.Base64;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * encryption util
+ * Created by wangzhen on 2020/8/19.
+ */
+public class EncryptionUtil {
+    public static String getMD5(String s) {
+        char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F'};
+        byte[] btInput = s.getBytes();
+        MessageDigest mdInst = null;
+        try {
+            mdInst = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        mdInst.update(btInput);
+        byte[] md = mdInst.digest();
+        int j = md.length;
+        char[] str = new char[j * 2];
+        int k = 0;
+        for (byte b : md) {
+            str[k++] = hexDigits[b >>> 4 & 0xf];
+            str[k++] = hexDigits[b & 0xf];
+        }
+        return new String(str);
+    }
+
+    public static String getMD5Small(String s) {
+        return getMD5(s).toLowerCase();
+    }
+
+    public static String getSHA256(String s) {
+        MessageDigest messageDigest;
+        String encodeStr = "";
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(s.getBytes("UTF-8"));
+            encodeStr = byte2Hex(messageDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encodeStr;
+    }
+
+    private static String byte2Hex(byte[] bytes) {
+        StringBuffer stringBuffer = new StringBuffer();
+        String temp = null;
+        for (int i = 0; i < bytes.length; i++) {
+            temp = Integer.toHexString(bytes[i] & 0xFF);
+            if (temp.length() == 1) {
+                //1得到一位的进行补0操作
+                stringBuffer.append("0");
+            }
+            stringBuffer.append(temp);
+        }
+        return stringBuffer.toString();
+    }
+
+    /**
+     * OkHttp header do not support: null、\n or 中文
+     *
+     * @param value value
+     * @return data
+     */
+    public static String getValueEncoded(String value) {
+        if (value == null) return "null";
+        String newValue = value.replace("\n", "");
+        for (int i = 0, length = newValue.length(); i < length; i++) {
+            char c = newValue.charAt(i);
+            if (c <= '\u001f' || c >= '\u007f') {
+                try {
+                    return URLEncoder.encode(newValue, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return newValue;
+    }
+
+    public static String AesDecrypt(String src) {
+        try {
+            IvParameterSpec ivSpec = new IvParameterSpec("FRJ9op4MKgTvfBEA".getBytes());
+            byte[] raw = "Kw9d$wx[NNL^$9qd".getBytes();
+            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivSpec);
+            byte[] encrypted1 = Base64.decode(src, Base64.DEFAULT);
+            byte[] original = cipher.doFinal(encrypted1);
+            return new String(original, "utf-8");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
